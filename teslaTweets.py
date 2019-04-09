412396e01d64a735cab72ebaf8b35108bdf84e11
Epifanio Jimenez <epijimenez@Epis-MacBook-Pro.local>
2019-04-09 13:14:46 -0700

    made various changes; included check for tire rotation, brake fluid, battery coolant, and changing the way the logging occurs

--- teslaTweets.py
+++ teslaTweets.py
@@ -32,20 +32,24 @@ LOGGER = None
 #	********************************************************************	#
 
 
-def establish_connection(token=None):
+def t_setup():
+	c = establish_connection()
+	userCar= get_car(c)
+	return c, userCar
+
+def establish_connection():
 	userEmail = TESLA_EMAIL
 	userPassword = TESLA_PASSWORD
-
 	for x in range(1, 4):
 		try:
 			c = teslajson.Connection(userEmail, userPassword)
 			return c
 		except urllib2.HTTPError as e:
-			LOGGER.error("\n>>>>> Wrong email or password. Tried {} times. Retrying in 60 seconds...\nError: {}\n".format(x, e))
+			write_log('error', "Wrong email or password. Tried {} times. Retrying in 60 seconds. Error: {}".format(x, e))
 			time.sleep(60)
 			x =+ 1
+	write_log('error', "Can not connect to Tesla account. Verify credentials.")
 	return False
-
 
 def get_car(c):
 	userCar = TESLA_CAR
@@ -58,13 +62,13 @@ def get_car(c):
 					time.sleep(10)
 					return str(userCar)
 				except urllib2.HTTPError as e:
-					LOGGER.error("\n>>>>> Unable to contact {}. Tried {} times. Retrying in 60 seconds...\nError: {}\n".format(car, x, e))
+					write_log('error', "Unable to contact {}. Tried {} times. Retrying in 60 seconds. Error: {}".format(car, x, e))
 					x += 1
 			else:
-				LOGGER.error("\n>>>>> Couldn't find {} in your garage.\n".format(userCar))
+				write_log('error', "Couldn't find {} in your garage.\n".format(userCar))
 				return False
+	write_log('error', "Can not find car in Tesla account. Verify credentials.")
 	return False
-
 
 def wakeup_car(c, car):
 	for x in range(1, 4):
@@ -75,43 +79,47 @@ def wakeup_car(c, car):
 					time.sleep(10)
 					return True
 				except urllib2.HTTPError as e:
-					LOGGER.error("\n>>>>> Unable to contact {}. Tried {} times. Retrying in 60 seconds...\nError: {}\n".format(car, x, e))
+					write_log('error', "Unable to contact {}. Tried {} times. Retrying in 60 seconds. Error: {}".format(car, x, e))
 					x += 1
 					time.sleep(60)
 	return False
 
-
 def monitor_odometer(c, car):
-	MILES_MILESTONE = int(read_miles_milestone())
-	odometer = None
-
+	write_log('log', "Checking odometer...")
+	try:
+		MILES_MILESTONE = int(read_log('milestone'))
+	except Exception as e:
+		write_log('error', "No milestone in log. Recording current milage")
+		MILES_MILESTONE = 0
+	odometer = False
 	wakeup_car(c, car)
-
+	
 	for v in c.vehicles:
 		if v["display_name"] == car:
 			try:
 				d = v.data_request("vehicle_state")
 				odometer = int(d["odometer"])
 			except urllib2.HTTPError as e:
-				LOGGER.error("\n>>>>> Unable to get odometer reading.\nError: {}\n".format(e))
+				write_log('error', "Unable to get odometer reading. Error: {}".format(e))
 				odometer = False
-			
+	
 	if odometer:
 		odometer_r = int(round((odometer-500), -3))
-		
-		LOGGER.info("\n[X] Checked odometer: {}. Using rounded number: {}\n".format(odometer, odometer_r))
-		
+		write_log('log', "Checked odometer: {}".format(odometer))
+		if (MILES_MILESTONE == 0):
+			write_log('milestone', str(odometer_r))
+			return False
 		if (odometer >= MILES_MILESTONE):
-			tweet(c, car, "Let's go! Today I passed {} miles!".format(odometer_r))
-			change_miles_milestone(str(odometer_r+1000))
-			return True
+			if tweet(c, car, "Let's go! Today I passed {} miles!".format(odometer_r)):
+				write_log('milestone', str(odometer_r+1000))
+				return True
 
 	return False
 
-
 def monitor_charging(c, car):
-	global CHARGE_STATUS
-	charge_state = None
+	write_log('log', "Checking charging state...")
+	prev_charge_state = str(read_log('charge'))
+	current_charge_state = False
 	miles = None
 
 	wakeup_car(c, car)
@@ -120,29 +128,26 @@ def monitor_charging(c, car):
 		if v["display_name"] == car:
 			try:
 				d = v.data_request("charge_state")
-				charge_state = d["charging_state"]
+				current_charge_state = d["charging_state"]
 				miles = int(d["ideal_battery_range"])
 				percentage = int(d["usable_battery_level"])
 			except urllib2.HTTPError as e:
-				LOGGER.error("\n>>>>> Unable to get charging state.\nError: {}\n".format(e))
-				charge_state = False
+				write_log('error', "Unable to get charging state. Error: {}".format(e))
+				current_charge_state = False
 
-	if charge_state:
-		LOGGER.info("\n[X] Checked charging status: {}\n".format(charge_state))
-
-		if (charge_state == "Complete") and (CHARGE_STATUS != 2) and (percentage >= 75):
-			tweet(c, car, "Charged up to {}%! Ready to go with {} miles available.".format(percentage, miles))
-			CHARGE_STATUS = 2
-			return True
-		elif (charge_state == "Charging") and (CHARGE_STATUS != 1):
-			tweet(c, car, "Currently charging my battery... Charged to {}%.".format(percentage))
-			CHARGE_STATUS = 1
-			return True
-		elif (charge_state != "Complete") and (charge_state != "Charging"):
-			CHARGE_STATUS = 0
-	
+	if current_charge_state:
+		write_log('log', "Checked charging status: {}".format(current_charge_state))
+		if (current_charge_state == "Complete") and (prev_charge_state != "Complete") and (percentage >= 75):
+			if tweet(c, car, "Charged up to {}%! Ready to go with {} miles available.".format(percentage, miles)):
+				write_log('charge', current_charge_state)
+				return True
+		elif (current_charge_state == "Charging") and (prev_charge_state != "Charging"):
+			if tweet(c, car, "Currently charging my battery... Charged to {}%.".format(percentage)):
+				write_log('charge', current_charge_state)
+				return True
+		elif (current_charge_state != "Complete") and (current_charge_state != "Charging") and (prev_charge_state != "Disconnected"):
+			write_log('charge', current_charge_state)
 	return False
-
 
 def monitor_temp(c, car):
 	global YEAR_DAY
@@ -156,37 +161,107 @@ def monitor_temp(c, car):
 				d = v.data_request('climate_state')
 				outside_temp_c = int(d["outside_temp"])
 			except urllib2.HTTPError as e:
-				LOGGER.error("\n>>>>> Unable to get temperature.\nError: {}\n".format(e))
+				write_log('error', "Unable to get temperature.\nError: {}\n".format(e))
 				outside_temp_c = False
 
 	if outside_temp_c:
 		outside_temp_f = (((outside_temp_c * 9) / 5) + 35 )
 
-		LOGGER.info("\n[X] Checked temperature: {}C\n".format(outside_temp_c))
+		write_log('log', "[X] Checked temperature: {}C\n".format(outside_temp_c))
 
 		if (outside_temp_f < 40) and (YEAR_DAY != int(time.strftime("%j"))):
-			tweet(c, car, "It's really cold... It's currently {}F / {}C. Bring a jacket.".format(int(outside_temp_f), int(outside_temp_c)))
-			YEAR_DAY = int(time.strftime("%j"))
-			return True
+			if tweet(c, car, "It's really cold... It's currently {}F / {}C. Bring a jacket.".format(int(outside_temp_f), int(outside_temp_c))):
+				YEAR_DAY = int(time.strftime("%j"))
+				return True
 		elif (outside_temp_f < 60) and (YEAR_DAY != int(time.strftime("%j"))):
-			tweet(c, car, "Baby is cold outside... It's currently {}F / {}C. Stay warm.".format(int(outside_temp_f), int(outside_temp_c)))
-			YEAR_DAY = int(time.strftime("%j"))
-			return True
+			if tweet(c, car, "Baby is cold outside... It's currently {}F / {}C. Stay warm.".format(int(outside_temp_f), int(outside_temp_c))):
+				YEAR_DAY = int(time.strftime("%j"))
+				return True
 		elif (outside_temp_f > 90) and (YEAR_DAY != int(time.strftime("%j"))):
-			tweet(c, car, "It's getting hot in here... It's currently {}F / {}C.".format(int(outside_temp_f), int(outside_temp_c)))
-			YEAR_DAY = int(time.strftime("%j"))
-			return True
+			if tweet(c, car, "It's getting hot in here... It's currently {}F / {}C.".format(int(outside_temp_f), int(outside_temp_c))):
+				YEAR_DAY = int(time.strftime("%j"))
+				return True
 		elif (outside_temp_f > 100) and (YEAR_DAY != int(time.strftime("%j"))):
-			tweet(c, car, "Wow, calm down there sun! It's currently {}F / {}C. Drink water.".format(int(outside_temp_f), int(outside_temp_c)))
-			YEAR_DAY = int(time.strftime("%j"))
-			return True
+			if tweet(c, car, "Wow, calm down there sun! It's currently {}F / {}C. Drink water.".format(int(outside_temp_f), int(outside_temp_c))):
+				YEAR_DAY = int(time.strftime("%j"))
+				return True
 
 	return False
 
+# TODO: Monitor Tire Rotation every ~6000 miles
+def monitor_maintenance(c, car):
+	write_log('log', "Checking maintenance...")
+	maintenance_schedule = {'tire_rotation': 6250, 'brake_fluid': 25000, 'battery_coolant': 50000}
+
+	try:
+		last_tire_rotation = int(read_log('maintenance_tr'))
+	except Exception as e:
+		write_log('error', "No tire maintenance in log. Starting new.")
+		write_log('maintenance_tr', 0)
+		last_tire_rotation = 0
+	try:
+		last_brake_fluid = int(read_log('maintenance_bf'))
+	except Exception as e:
+		write_log('error', "No brake fluid maintenance in log. Starting new.")
+		write_log('maintenance_bf', 0)
+		last_brake_fluid = 0
+	try:
+		last_battery_coolant = int(read_log('maintenance_bc'))
+	except Exception as e:
+		write_log('error', "No battery coolant maintenance in log. Starting new.")
+		write_log('maintenance_bc', 0)
+		last_battery_coolant = 0
+
+	odometer = False
+	wakeup_car(c, car)
+
+	for v in c.vehicles:
+		if v["display_name"] == car:
+			try:
+				d = v.data_request("vehicle_state")
+				odometer = int(d["odometer"])
+			except urllib2.HTTPError as e:
+				write_log('error', "Unable to get odometer reading. Error: {}".format(e))
+				odometer = False
+
+	last_tire_rotation_delta = odometer - last_tire_rotation
+	last_brake_fluid_delta = odometer - last_brake_fluid
+	last_battery_coolant_delta = odometer - last_battery_coolant
+
+	tweet_sent = False
+	logChecks = ""
+
+	if odometer:
+		if last_tire_rotation_delta >= maintenance_schedule['tire_rotation']:
+			write_log('maintenance_tr', (odometer))
+			logChecks += "[X]TireRotation "
+			if tweet(c, car, "Time to do tire rotation! {} miles have already passed.".format(int(last_tire_rotation_delta))):
+				rettweet_sent = True
+		else:
+			logChecks += "[ ]TireRotation "
+		if last_brake_fluid_delta >= maintenance_schedule['brake_fluid']:
+			write_log('maintenance_bf', (odometer))
+			logChecks += "[X]BrakeFluid "
+			if tweet(c, car, "Time to change that brake fluid! {} miles have already passed.".format(int(last_brake_fluid_delta))):
+				tweet_sent = True
+		else:
+			logChecks += "[ ]BrakeFluid "
+		if last_battery_coolant_delta >= maintenance_schedule['battery_coolant']:
+			write_log('maintenance_bc', (odometer))
+			logChecks += "[X]BatteryCoolant "
+			if tweet(c, car, "WOW! Time to change the battery coolant!! {} miles have already passed.".format(int(last_battery_coolant_delta))):
+				tweet_sent = True
+		else:
+			logChecks += "[ ]BatteryCoolant "
+		write_log('log', "Checked maintenance. {}".format(logChecks))
+	
+	if tweet_sent:
+		return True
+
+	return False
 
 def get_location(c, car):
 	#location = None
-
 	wakeup_car(c, car)
 
 	for v in c.vehicles:
@@ -196,7 +271,7 @@ def get_location(c, car):
 				latitude = round(d["latitude"], 2)
 				longitude = round(d["longitude"], 2)
 			except urllib2.HTTPError as e:
-				LOGGER.error("\n>>>>> Unable to get location.\nError: {}\n".format(e))
+				write_log('error', "Unable to get location.\nError: {}\n".format(e))
 				latitude = False
 				latitude = False
 	#geolocator = Nominatim(user_agent="tesla reporter")
@@ -209,100 +284,94 @@ def get_location(c, car):
 	# Leaving this here in case is needed in future. Will need to import " from geopy.geocoders import Nominatim ".
 	return latitude, longitude
 
-
 def tweet(c, car, message = "Opps... No message to broadcast for now! Have a good day!"):
 	location = get_location(c, car)
 
-	if location[0] or location[1]:
-		for x in range(1,4):
-			try:
-				TWITTER.update_status(status= "{} | {}".format(message, HASHTAGS)) 
-				LOGGER.info("\n>>>>> Posted to Twitter (No Location): {}\n".format(message))
-				break
-			except Exception as e:
-				LOGGER.error("\n>>>>> Error posting to Twitter. Tried {} time(s). Trying again... Error: {}\n".format(x, e))
-	else:
+	if location[0] and location[1]:
 		for x in range(1,4):
 			try:
 				TWITTER.update_status(status= "{} | {}".format(message, HASHTAGS), lat= location[0], long= location[1]) 
-				LOGGER.info("\n>>>>> Posted to Twitter (With Location): {}\n".format(message))
+				write_log('log', "Tweet (With Location): {}".format(message))
+				return True
 				break
 			except Exception as e:
-				LOGGER.error("\n>>>>> Error posting to Twitter. Tried {} time(s). Trying again... Error: {}\n".format(x, e))
+				if 'duplicate' in str(e):
+					write_log('error', "Twitter posting. Tries ({}). Error: {}".format(x, 'Duplicate Tweet'))
+					return False
+					break
+				else:
+					write_log('error', "Twitter posting. Tries ({}). Error: {}".format(x, e))
+					return False
+	else:
+		for x in range(1,4):
+			try:
+				TWITTER.update_status(status= "{} | {}".format(message, HASHTAGS)) 
+				write_log('log', "Tweet (No Location): {}".format(message))
+				return True
+				break
+			except Exception as e:
+				if 'duplicate' in str(e):
+					write_log('error', "Twitter posting. Tries ({}). Error: {}".format(x, 'Duplicate Tweet'))
+					return False
+					break
+				else:
+					write_log('error', "Twitter posting. Tries ({}). Error: {}".format(x, e))
+					return False
 
+def read_log(lookup=None):
+	validData = ['create', 'milestone', 'maintenance_tr', 'maintenance_bf', 'maintenance_bc', 'charge', 'error', 'log']
+	if lookup in validData:
+		try:
+			with open("TeslaLog.txt", "r") as f:
+				lines = f.readlines()
+				#return lines[-1]
+				for i in reversed(lines):
+					if lookup in i:
+						return i.split("/")[-1].strip()
+		except IOError:
+			write_log('create')
+			return False
+	else:
+		return False
 
-def setup_logging():
-	global LOGGER
-	LOGGER = logging.getLogger('TeslaTweets')
-	hdlr = logging.FileHandler('TeslaTweets.log')
-	formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-	hdlr.setFormatter(formatter)
-	LOGGER.addHandler(hdlr)
-	LOGGER.setLevel(logging.INFO)
-
-
-def read_miles_milestone():
-	try:
-		with open("MILES_MILESTONE.txt", "r") as f:
-			lines = f.readlines()
-			return lines[-1]
-	except IOError:
-		change_miles_milestone("0")
-		return "0"
-
-def change_miles_milestone(newMilestone):
-	try:
-		with open("MILES_MILESTONE.txt", "a") as f:
-			f.write("\n{}".format(str(newMilestone)))
-	except Exception as e:
-		LOGGER.error("\n>>>>> Can not write MILES_MILESTONE.txt file. Check permission on folder.\nError: {}".format)
+def write_log(writeup=None, data=None):
+	validData = ['create', 'milestone', 'maintenance_tr', 'maintenance_bf', 'maintenance_bc', 'charge', 'error', 'log']
+	if writeup in validData:
+		try:
+			with open("TeslaLog.txt", "a") as f:
+				f.write("\n{}: {}/{}".format(time.asctime(), writeup, str(data)))
+		except Exception as e:
+			return False
+	else:
+		return False
 
 def main():
 	global TWITTER
 	TWITTER = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
-	setup_logging()
 	
-	c = establish_connection()
-	if not c:
-		LOGGER.error("\n>>>>> Wrong email or password. Please verify your Tesla Account credentials.")
-	LOGGER.info("\n>>>>> Connected to Tesla account sucessfully!\n")
+	c, userCar = t_setup()
+	if c and userCar:
+		write_log('log', "Connected to {} successfully!".format(userCar))
+	else:
+		return 0
+	logTweets = ""
 
-	userCar = get_car(c)
-	if not userCar:
-		LOGGER.error("\n>>>>> Unable to find your car. Please verify your information.")
-	LOGGER.info("\n>>>>> Connected to {} sucessfully!\n".format(userCar))
+	if monitor_odometer(c, userCar):
+		logTweets += "[X]odometer "
+	else:
+		logTweets += "[ ]odometer "
 
-	while True:
-		logChecks = ""
-		logTweets = ""
-
-		LOGGER.info("\n[ ] Checking odomenter...\n")
-		logChecks += "[X]odometer "
-		if monitor_odometer(c, userCar):
-			LOGGER.info("\n[X] Tweeted odometer.\n")
-			logTweets += "[X]odometer "
-		else:
-			logTweets += "[ ]odometer "
-
-		LOGGER.info("\n[ ] Checking charging state...\n")
-		logChecks += "[X]charging "
-		if monitor_charging(c, userCar):
-			LOGGER.info("\n[X] Tweeted charging state.\n")
-			logTweets += "[X]charging "
-		else:
-			logTweets += "[ ]charging "
-		
-		LOGGER.info("\n[ ] Checking temperature...\n")
-		logChecks += "[X]temperature "
-		if monitor_temp(c, userCar):
-			LOGGER.info("\n[X] Tweeted temperature.\n")
-			logTweets += "[X]temperature "
-		else:
-			logTweets += "[ ]temperature "
-
-		LOGGER.info("\n>>>>> Going to sleep for 60 minutes...\nCheck: {}\nTweets: {}\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n".format(logChecks, logTweets))
-		time.sleep(3600)
+	if monitor_charging(c, userCar):
+		logTweets += "[X]charging "
+	else:
+		logTweets += "[ ]charging "
+	
+	if monitor_maintenance(c, userCar):
+		logTweets += "[X]maintenance "
+	else:
+		logTweets += "[ ]maintenance "
+	
+	write_log('log', "Finished! Tweets: {}\n".format(logTweets))
 
 main()
 

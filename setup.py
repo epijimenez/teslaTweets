47622c9bdc64163a51f8e702d4751785afbeed36
Epi Jimenez <epi@apple.com>
2024-04-22 22:20:48 -0500

    adding packages keys

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup
-import teslatweet
 
 setup(
     name='teslatweet',
-    version='0.2.4',
+    version='0.2.5',
     py_modules=['teslatweet'],
     python_requires=">=3.9",
     install_requires=["TeslaPy", "googlemaps", "DateTime", "requests_oauthlib", "requests"],
     dependency_links=[
         "https://pypi.org/project/"
     ],
+    packages=['teslatweet'],
+    include_package_data=True,
+    package_dir={'teslatweet': 'teslatweet'},
+    package_data={'teslatweet': ['*']},
     entry_points={
         'console_scripts': [
             'teslatweet=teslatweet:main'
